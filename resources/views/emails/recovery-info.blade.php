4d754955ddea227629d7a27f62ac90e1dd2e0aa3
unknown <junicodefire@gmail.com>
2019-09-24 17:49:22 +0100

    Auth complete

--- resources/views/emails/recovery-info.blade.php
+++ resources/views/emails/recovery-info.blade.php
@@ -5,6 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Hymenaios New Password</title>
+        <style type="text/css">
+            p {
+                color: white !important;
+            }
+        </style>
         </head>
             <body>
     <section style="width: 100%;margin: auto;height:auto; padding-bottom:20px;box-shadow: 0 0 10px #f58731;color: grey; border-radius: 5px;background: #2b2b2b;">
@@ -14,7 +19,7 @@
 
         <div id="box" style="width: 95%; margin: auto; color: white; padding: 10px; background: #2b2b2b;"><br>
             <h4>Hello <b>{{$user->username}} --- [<span style="color: hotpink !important;">{{$user->email}}</span>]</b></h4>
-            <div id="third_block">
+            <div id="third_block" style="color: white;">
                     <p>Please copy the code below to change your password!</p><br><br>
                     <p style="text-align:center; background: #0094E7; width: 20%; height: 30px; padding-top: 10px; margin: auto; font-size: 18px; font-weight: bold;">{{ $user->verifycode }}</p>
                      <p><small style="font-size: 16px;">Please copy verification code and follow the <a href="https://app-hymenaios.firebasewebapp.com/reset.html">link</a> to change password!</small></p>
