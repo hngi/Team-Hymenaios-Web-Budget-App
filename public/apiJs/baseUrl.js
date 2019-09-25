46c37ac2e8f4cd1ad920b6bfaf8605a258572631
unknown <junicodefire@gmail.com>
2019-09-25 10:15:23 +0100

    Called login api

--- public/apiJs/baseUrl.js
+++ public/apiJs/baseUrl.js
@@ -1,1 +1,9 @@
 const baseUrl = `https://budget-calculator-app-api.herokuapp.com/`;
+//Logout User
+const logOutBtn = document.querySelector('[data-logout]')
+const logOut = () => {
+    localStorage.removeItem('h-user-data');
+    location.href='login.html'
+}
+
+logOutBtn.addEventListener('click', (event) => logOut(event));
