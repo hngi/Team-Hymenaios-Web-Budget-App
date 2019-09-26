591bb16799ed24b3458074cfcbca99722b987c02
cemma2035 <emmaplaysdestiny@gmail.com>
2019-09-26 15:08:00 +0100

    dashboard

--- public/apiJs/verify_account.js
+++ public/apiJs/verify_account.js
@@ -83,7 +83,7 @@ const verifyAccountFunc = (event) => {
 				error_field.style.color = 'white';
 				setTimeout( () => {
 					localStorage.setItem('h-user-data', JSON.stringify(data));
-					location.replace('dashboard.html');
+					location.replace('profile.html');
 				}, 3000)
 			}
 		})
