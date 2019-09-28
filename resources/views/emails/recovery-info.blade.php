ed69aa6f0a61221b964a2f9f7d29efc1c202a83d
unknown <junicodefire@gmail.com>
2019-09-28 19:36:08 +0100

    enhh

--- resources/views/emails/recovery-info.blade.php
+++ resources/views/emails/recovery-info.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hymenaios New Password</title>
+        <title>blueBOX New Password</title>
         <style type="text/css">
             p {
                 color: white !important;
@@ -17,7 +17,7 @@
             <body>
     <section style="width: 100%;margin: auto;height:auto; padding-bottom:20px;box-shadow: 0 0 10px #f58731;color: grey; border-radius: 5px;background: #2b2b2b;">
         <div id="head_1" style="background: #f58731; height: 80px;">
-         <h2 style="margin: 0;padding: 25px;color: white;background: #0094E7;font-family:sans-serif;font-weight: bold;">Hymenaios Budget App</h2><br><br>
+         <h2 style="margin: 0;padding: 25px;color: white;background: #0094E7;font-family:sans-serif;font-weight: bold;">blueBOX Budget App</h2><br><br>
         </div>
 
         <div id="box" style="width: 95%; margin: auto; color: white; padding: 10px; background: #2b2b2b;"><br>
@@ -26,7 +26,7 @@
                     <p>Please copy the code below to change your password!</p><br><br>
                     <p style="text-align:center; background: #0094E7; width: 20%; height: 30px; padding-top: 10px; margin: auto; font-size: 18px; font-weight: bold;">{{ $user->verifycode }}</p>
                      <p><small style="font-size: 16px;">Please copy verification code and follow the <a href="https://app-hymenaios.firebasewebapp.com/reset.html">link</a> to change password!</small></p>
-                    <h5 style="text-align:right;">Team Hymenaios</h5>
+                    <h5 style="text-align:right;">Team blueBOX</h5>
                     <p style="text-align:left;">You are getting this email because you have requested to change your password, if this was not authorize by you please discard.</p>
             </div>
         </div>
